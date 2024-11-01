6dc1879bcdf8dd714ff30f06d7cd9da4a0e24f39
aashayarora <aaarora@ucsd.edu>
2024-11-01 09:47:27 -0700

    refactored rucio daemons

--- src/dmm/daemons/rucio/initializer.py
+++ src/dmm/daemons/rucio/initializer.py
@@ -14,27 +14,35 @@ class RucioInitDaemon(DaemonBase):
     def process(self, client=None, session=None):
         rules = client.list_replication_rules()
         for rule in rules:
-            if Request.from_id(rule["id"], session=session) is not None:
+            if self._is_rule_in_db(rule, session):
                 logging.debug(f"rule {rule['id']} already in db, nothing to do")
                 continue
-            else:
-                logging.debug(f"evaluating rule {rule['id']}")
-                if ((rule["meta"] is None) and ("sense" not in rule["meta"])):
-                    logging.debug(f"rule {rule['id']} is not a sense rule, will add to db but do nothing")
-                    new_request = Request(rule_id=rule["id"],
-                                            src_site=Site.from_name(rule["source_replica_expression"], session=session), 
-                                            dst_site=Site.from_name(rule["rse_expression"], session=session),
-                                            priority=rule["priority"],
-                                            transfer_status="NOT_SENSE",
-                                            fts_limit_desired=20,
-                                        )
-                else:
-                    logging.debug(f"rule {rule['id']} identified as a sense rule")
-                    new_request = Request(rule_id=rule["id"],
-                                            src_site=Site.from_name(rule["source_replica_expression"], session=session), 
-                                            dst_site=Site.from_name(rule["rse_expression"], session=session),
-                                            priority=rule["priority"],
-                                            transfer_status="INIT",
-                                            fts_limit_desired=20,
-                                        )
-                    new_request.save(session=session)
+
+            logging.debug(f"evaluating rule {rule['id']}")
+            new_request = self._create_request_from_rule(rule, session)
+            new_request.save(session=session)
+
+    def _is_rule_in_db(self, rule, session):
+        return Request.from_id(rule["id"], session=session) is not None
+
+    def _create_request_from_rule(self, rule, session):
+        src_site = Site.from_name(rule["source_replica_expression"], session=session)
+        dst_site = Site.from_name(rule["rse_expression"], session=session)
+        priority = rule["priority"]
+        fts_limit_desired = 20
+
+        if rule["meta"] is None or "sense" not in rule["meta"]:
+            logging.debug(f"rule {rule['id']} is not a sense rule, will add to db but do nothing")
+            transfer_status = "NOT_SENSE"
+        else:
+            logging.debug(f"rule {rule['id']} identified as a sense rule")
+            transfer_status = "INIT"
+
+        return Request(
+            rule_id=rule["id"],
+            src_site=src_site,
+            dst_site=dst_site,
+            priority=priority,
+            transfer_status=transfer_status,
+            fts_limit_desired=fts_limit_desired,
+        )
--- src/dmm/daemons/rucio/modifier.py
+++ src/dmm/daemons/rucio/modifier.py
@@ -12,15 +12,20 @@ class RucioModifierDaemon(DaemonBase):
     @databased
     def process(self, client=None, session=None):
         reqs = Request.from_status(status=["ALLOCATED", "STAGED", "DECIDED", "PROVISIONED"], session=session)
-        if reqs == []:
+        if not reqs:
             return
+        
         req_prio_changed = False
         for req in reqs:
             curr_prio_in_rucio = client.get_replication_rule(req.rule_id)["priority"]
             if req.priority != curr_prio_in_rucio:
                 req_prio_changed = True
-                logging.debug(f"{req.rule_id} priority changed from {req.priority} to {curr_prio_in_rucio}")
-                req.update_priority(priority=curr_prio_in_rucio, session=session)
-                req.mark_as(status="MODIFIED", session=session)
+                self._update_request_priority(req, curr_prio_in_rucio, session)
+        
         if not req_prio_changed:
             logging.debug("No priority changes detected in rucio")
+
+    def _update_request_priority(self, req, new_priority, session):
+        logging.debug(f"{req.rule_id} priority changed from {req.priority} to {new_priority}")
+        req.update_priority(priority=new_priority, session=session)
+        req.mark_as(status="MODIFIED", session=session)
